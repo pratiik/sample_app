131ebd46e6e5f2f924a1b39ae1b0729bdabe1db5
Pratiik Atrii <prateek.attree@addvalsolutions.com>
2013-12-05 19:03:07 +0530

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,18 @@ class UsersController < ApplicationController
 		redirect_to(root_path) unless current_user?(@user)
 	end
 	def admin_user
-      redirect_to(root_path) unless current_user.admin?
-    end
+		redirect_to(root_path) unless current_user.admin?
+	end
+	def following
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.followed_users.paginate(page: params[:page])
+		render 'show_follow'
+	end
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
+	end
 end
